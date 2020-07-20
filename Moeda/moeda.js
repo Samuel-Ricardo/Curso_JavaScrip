a5ae46547f3b3b752ac3441393956582392626df
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-07-20 18:07:24 -0300

    converter moeda

---
+++ Moeda/moeda.js
@@ -0,0 +1,14 @@
+
+//converte para moeda Real BR
+
+            var n1 = 12.53
+
+            n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+
+           // converte para moeda Dolar USD
+
+            n1.toLocaleString('pt-BR', {style: 'currency', currency: 'USD'})
+
+           // converte para moeda Euro EUR
+
+            n1.toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'})
