c82dfe883bc7a3b7f90d6cd85d828b35f3cb9784
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-09-11 20:34:37 -0300

    refatoring

---
+++ Atividade Repetiçao/Atividade 01/Controller/PageController.js
@@ -0,0 +1,108 @@
+function startCount(){
+
+    var startInput = document.querySelector("input#startNumber");
+    var intervalInput = document.querySelector("input#interval");
+    var finalInput = document.querySelector("input#finalNumber");
+
+    var result = document.getElementById("result");
+
+        var start = Number(startInput.value)
+        var interval = Number(intervalInput.value)
+        var final = Number(finalInput.value)
+    
+    if(check() === true){
+
+        var count = "";
+
+        result.innerText = `contando:`
+
+        if(start < final){
+
+           // for(start; start != final; start++){
+
+                //    count = count + `👍 ${start} \u{1f449} `
+        
+             //        count = count + ` ${start} \u{1f449} `
+             //   }
+
+             count =   increasing(start, final, interval)
+
+        }else{
+
+            //for(start; start != final; start--){
+
+                //    count = count + `👍 ${start} \u{1f449} `
+        
+             //        count = count + ` ${start} \u{1f449} `
+              //  }
+              count =   decreasing(start, final, interval)
+            }
+
+        count = count + `\u{1f3c1} ${final} `
+
+        result.innerHTML  = count
+    }else{
+
+        result.innerText = `dados incompletos ;-;`
+    }
+}
+
+function increasing(start, final, interval){
+
+    var count = ""
+
+    for(start; start != final; start += interval){
+
+        //    count = count + `👍 ${start} \u{1f449} `
+
+             count = count + ` ${start} \u{1f449} `
+        }
+
+        return count;
+}
+
+function decreasing(start, final, interval){
+
+    var count = ""
+
+    for(start; start != final; start -= interval){
+
+        //    count = count + `👍 ${start} \u{1f449} `
+
+             count = count + ` ${start} \u{1f449} `
+        }
+
+        return count;
+}
+
+function check(){
+
+    var startInput = document.querySelector("input#startNumber");
+    var intervalInput = document.querySelector("input#interval")
+    var finalInput = document.querySelector("input#finalNumber")
+
+     if(startInput.value.length == 0){
+
+        window.alert(`[Erro] voce nao preencheu o valor inicial`)
+
+        return false;
+     }
+
+     else if(finalInput.value.length == 0){
+
+        window.alert(`[ERRO] Voce nao digitou o valor final ;-;`);
+        return false;
+     }
+     
+     else if(intervalInput.value.length == 0 || interval <= 0){
+
+        window.alert(`voce nao preencheu o intervalo, será ultilizado o "1" por padrao`)
+
+        interval = 1;
+        return true
+     }
+     else{
+         return true
+     }
+
+    }
