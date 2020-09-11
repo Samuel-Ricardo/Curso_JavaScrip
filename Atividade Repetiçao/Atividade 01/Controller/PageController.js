b9dda2e4999d5254ea4989cabe7fbf4452cde529
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-09-11 19:17:20 -0300

    Created: function check()

--- Atividade Repetiçao/Atividade 01/Controller/PageController.js
+++ Atividade Repetiçao/Atividade 01/Controller/PageController.js
@@ -6,18 +6,56 @@ function startCount(){
 
     var result = document.getElementById("result");
 
-    var start = Number(startInput.value)
-    var interval = Number(intervalInput.value)
-    var final = Number(finalInput.value)
+        var start = Number(startInput.value)
+        var interval = Number(intervalInput.value)
+        var final = Number(finalInput.value)
+    
+    if(check() === true){
 
-    var count = "contando"
+        
 
-    result.innerText = `contando:`
+        var count = "contando"
 
-    for(start; start != final; start++){
+        result.innerText = `contando:`
 
-        count = count + `👍 ${start} `
+        for(start; start != final; start++){
+
+            count = count + `👍 ${start} `
+        }
+
+        result.innerHTML  = count
     }
+}
 
-    result.innerHTML = count
+function check(){
+
+    var startInput = document.querySelector("input#startNumber");
+    var intervalInput = document.querySelector("input#interval")
+    var finalInput = document.querySelector("input#finalNumber")
+
+     if(startInput.value.length == 0){
+
+        window.alert(`[Erro] voce nao preencheu o valor inicial`)
+
+        return false;
+     }
+
+     else if(finalInput.value.length == 0){
+
+        window.alert(`[ERRO] Voce nao digitou o valor final ;-;`);
+        return false;
+     }
+     
+     else if(intervalInput.value.length == 0){
+
+        window.alert(`voce nao preencheu o intervalo, será ultilizado o "1" por padrao`)
+
+        interval = 1;
+        return true
+     }
+     else{
+         return true
+     }
+
+     
 }
