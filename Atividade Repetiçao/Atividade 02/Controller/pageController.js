afab1663b71bdc0d82896f1de9e06e21d233f216
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-09-12 22:19:53 -0300

    gerarTabuada

--- Atividade Repetiçao/Atividade 02/Controller/pageController.js
+++ Atividade Repetiçao/Atividade 02/Controller/pageController.js
@@ -1,5 +1,25 @@
 function gerarTabuada(){
 
+    var txtNumber = document.querySelector("input#number")
+    var btn = document.getElementById("tabuada")
 
-    
+    var numer = Number(txtNumber.value)
+
+    if(txtNumber.value.length != 0){
+
+        var cont = 0
+
+        while(cont < 11){
+
+            var item = document.createElement(`option`)
+
+            item.text = `${number} x ${cont} = ${number*cont}`
+
+
+        }
+
+    }else{
+
+        window.alert("Insira um numero por favor");
+
 }
